761765c69b80ea0364a7b9f1a7e836e3125f23af
mheily <mheily@fb4e3144-bc1c-4b72-a658-5bcd248dd7f7>
2010-07-24 03:21:17 +0000

    fix evfilt_user and ev_dispatch
    git-svn-id: svn://svn.code.sf.net/p/libkqueue/code/trunk@261 fb4e3144-bc1c-4b72-a658-5bcd248dd7f7

--- test/user.c
+++ test/user.c
@@ -92,6 +92,46 @@ test_kevent_user_oneshot(void)
     test_no_kevents(kqfd);
 }
 
+#if HAVE_EV_DISPATCH
+void
+test_kevent_user_dispatch(void)
+{
+    struct kevent kev;
+
+    test_no_kevents(kqfd);
+
+    /* Add the event, and then trigger it */
+    kevent_add(kqfd, &kev, 1, EVFILT_USER, EV_ADD | EV_CLEAR | EV_DISPATCH, 0, 0, NULL);
+    kevent_add(kqfd, &kev, 1, EVFILT_USER, 0, NOTE_TRIGGER, 0, NULL);
+
+    /* Retrieve one event */
+    kev.fflags &= ~NOTE_FFCTRLMASK;
+    kev.fflags &= ~NOTE_TRIGGER;
+    kev.flags = EV_CLEAR;
+    kevent_cmp(&kev, kevent_get(kqfd));
+
+    /* Confirm that the knote is disabled automatically */
+    test_no_kevents(kqfd);
+
+    /* Re-enable the kevent */
+    /* FIXME- is EV_DISPATCH needed when rearming ? */
+    kevent_add(kqfd, &kev, 1, EVFILT_USER, EV_ENABLE | EV_CLEAR | EV_DISPATCH, 0, 0, NULL);
+    test_no_kevents(kqfd);
+
+    /* Trigger the event */
+    kevent_add(kqfd, &kev, 1, EVFILT_USER, 0, NOTE_TRIGGER, 0, NULL);
+    kev.fflags &= ~NOTE_FFCTRLMASK;
+    kev.fflags &= ~NOTE_TRIGGER;
+    kev.flags = EV_CLEAR;
+    kevent_cmp(&kev, kevent_get(kqfd));
+    test_no_kevents(kqfd);
+
+    /* Delete the watch */
+    kevent_add(kqfd, &kev, 1, EVFILT_USER, EV_DELETE, 0, 0, NULL);
+    test_no_kevents(kqfd);
+}
+#endif 	/* HAVE_EV_DISPATCH */
+
 void
 test_evfilt_user(int _kqfd)
 {
@@ -101,5 +141,8 @@ test_evfilt_user(int _kqfd)
     test(kevent_user_get);
     test(kevent_user_disable_and_enable);
     test(kevent_user_oneshot);
+#if HAVE_EV_DISPATCH
+    test(kevent_user_dispatch);
+#endif
     /* TODO: try different fflags operations */
 }
