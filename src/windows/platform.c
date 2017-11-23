e0ec221f2ea1c35c5ee72c001f648b6ce17eac37
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2017-11-23 16:21:16 +0000

    Trim trailing whitespace

--- src/windows/platform.c
+++ src/windows/platform.c
@@ -46,7 +46,7 @@ const struct kqueue_vtable kqops = {
 int
 windows_kqueue_init(struct kqueue *kq)
 {
-    kq->kq_iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 
+    kq->kq_iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL,
                                          (ULONG_PTR) 0, 0);
     if (kq->kq_iocp == NULL) {
         dbg_lasterror("CreateIoCompletionPort");
@@ -91,7 +91,7 @@ windows_kevent_wait(struct kqueue *kq, int no, const struct timespec *timeout)
 	int retval;
     DWORD       timeout_ms;
     BOOL        success;
-    
+
     if (timeout == NULL) {
         timeout_ms = INFINITE;
     } else if ( timeout->tv_sec == 0 && timeout->tv_nsec < 1000000 ) {
@@ -110,10 +110,10 @@ windows_kevent_wait(struct kqueue *kq, int no, const struct timespec *timeout)
 #if 0
     if(timeout_ms <= 0)
         dbg_printf("Woop, not waiting !?");
-#endif        
+#endif
     memset(&iocp_buf, 0, sizeof(iocp_buf));
-    success = GetQueuedCompletionStatus(kq->kq_iocp, 
-            &iocp_buf.bytes, &iocp_buf.key, &iocp_buf.overlap, 
+    success = GetQueuedCompletionStatus(kq->kq_iocp,
+            &iocp_buf.bytes, &iocp_buf.key, &iocp_buf.overlap,
             timeout_ms);
     if (success) {
         return (1);
@@ -153,7 +153,7 @@ windows_kevent_copyout(struct kqueue *kq, int nready,
      * Certain flags cause the associated knote to be deleted
      * or disabled.
      */
-    if (eventlist->flags & EV_DISPATCH) 
+    if (eventlist->flags & EV_DISPATCH)
         knote_disable(filt, kn); //TODO: Error checking
     if (eventlist->flags & EV_ONESHOT)
         knote_delete(filt, kn); //TODO: Error checking
