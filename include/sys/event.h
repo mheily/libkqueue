ba8670bcb19aec70b1cf35071e8eedd2f4dc6576
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2019-03-22 19:13:53 +0700

    Add doxygen markup for event.h

--- include/sys/event.h
+++ include/sys/event.h
@@ -46,137 +46,186 @@
 
 struct timespec;
 
-#define EVFILT_READ		(-1)
-#define EVFILT_WRITE		(-2)
-#define EVFILT_AIO		(-3)	/* attached to aio requests */
-#define EVFILT_VNODE		(-4)	/* attached to vnodes */
-#define EVFILT_PROC		(-5)	/* attached to struct proc */
-#define EVFILT_SIGNAL		(-6)	/* attached to struct proc */
-#define EVFILT_TIMER		(-7)	/* timers */
-#define EVFILT_NETDEV		(-8)	/* network devices */
-#define EVFILT_FS		(-9)	/* filesystem events */
-#define EVFILT_LIO		(-10)	/* attached to lio requests */
-#define EVFILT_USER		(-11)	/* User events */
-#define EVFILT_SYSCOUNT		11
-
-#define EV_SET(kevp_, a, b, c, d, e, f) do {	\
-	struct kevent *kevp = (kevp_);		\
-	(kevp)->ident = (a);			\
-	(kevp)->filter = (b);			\
-	(kevp)->flags = (c);			\
-	(kevp)->fflags = (d);			\
-	(kevp)->data = (e);			\
-	(kevp)->udata = (f);			\
+/** Populate a kevent structure in the <em>changelist</em>
+ *
+ * @param[in] _kevp    The kevent structure to populate.
+ * @param[in] _ident   The unique identifier for this event.
+ * @param[in] _filter  The unique identifier for this event.
+ * @param[in] _flags   Filter specific flags.
+ * @param[in] _data    Additional data for the filter.
+ * @param[in] _udata   Opaque user data identifier.  Not interpreted by libkqueue.
+ */
+#define EV_SET(_kevp, _ident, _filter, _flags, _fflags, _data, _udata) do { \
+    struct kevent *kevp = (_kevp);     \
+    (kevp)->ident = (_ident);          \
+    (kevp)->filter = (_filter);         \
+    (kevp)->flags = (_flags);          \
+    (kevp)->fflags = (_fflags);        \
+    (kevp)->data = (_data);            \
+    (kevp)->udata = (_udata);          \
 } while(0)
 
+/** Structure to hold an event registration or notification
+ *
+ * A list of kevent structures are passed in by the application as the <em>changelist</em>
+ * to set which notifications the application wishes to received.
+ *
+ * A list of kevent structures are passed back to the application as the <em>eventlist</em>
+ * to inform the application of events which occurred or filter states.
+ */
 struct kevent {
-	uintptr_t	ident;		/* identifier for this event */
-	short		filter;		/* filter for event */
-	unsigned short flags;
-	unsigned int fflags;
-	intptr_t	data;
-	void		*udata;		/* opaque user data identifier */
+    uintptr_t           ident;         //!< The unique identifier for this event.
+    short               filter;        //!< The unique identifier for this event.
+    unsigned short      flags;         //!< One or more of the EV_* macros or'd together.
+    unsigned int        fflags;        //!< Filter specific flags.
+    intptr_t            data;          //!< Additional data for the filter.
+    void                *udata;        //!< Opaque user data identifier.
 };
 
-/* actions */
-#define EV_ADD		0x0001		/* add event to kq (implies enable) */
-#define EV_DELETE	0x0002		/* delete event from kq */
-#define EV_ENABLE	0x0004		/* enable event */
-#define EV_DISABLE	0x0008		/* disable event (not reported) */
-
-/* flags */
-#define EV_ONESHOT	0x0010		/* only report one occurrence */
-#define EV_CLEAR	0x0020		/* clear event state after reporting */
-#define EV_RECEIPT	0x0040		/* force EV_ERROR on success, data=0 */
-#define EV_DISPATCH	0x0080		/* disable event after reporting */
-
-#define EV_SYSFLAGS	0xF000		/* reserved by system */
-#define EV_FLAG1	0x2000		/* filter-specific flag */
-
-/* returned values */
-#define EV_EOF		0x8000		/* EOF detected */
-#define EV_ERROR	0x4000		/* error, data contains errno */
-
- /*
-  * data/hint flags/masks for EVFILT_USER
-  *
-  * On input, the top two bits of fflags specifies how the lower twenty four
-  * bits should be applied to the stored value of fflags.
-  *
-  * On output, the top two bits will always be set to NOTE_FFNOP and the
-  * remaining twenty four bits will contain the stored fflags value.
-  */
-#define NOTE_FFNOP	0x00000000		/* ignore input fflags */
-#define NOTE_FFAND	0x40000000		/* AND fflags */
-#define NOTE_FFOR	0x80000000		/* OR fflags */
-#define NOTE_FFCOPY	0xc0000000		/* copy fflags */
-#define NOTE_FFCTRLMASK	0xc0000000		/* masks for operations */
-#define NOTE_FFLAGSMASK	0x00ffffff
-
-#define NOTE_TRIGGER	0x01000000		/* Cause the event to be
-						   triggered for output. */
-
-/*
- * data/hint flags for EVFILT_{READ|WRITE}
+/** @name Filters
+ *
+ * @{
  */
-#define NOTE_LOWAT	0x0001			/* low water mark */
-#undef  NOTE_LOWAT                  /* Not supported on Linux */
+#define EVFILT_READ          (-1)      //!< Read I/O event.
+#define EVFILT_WRITE         (-2)      //!< Write I/O event.
+#define EVFILT_AIO           (-3)      //!< Attached to aio requests.
+#define EVFILT_VNODE         (-4)      //!< Attached to vnodes.
+#define EVFILT_PROC          (-5)      //!< attached to struct proc.
+#define EVFILT_SIGNAL        (-6)      //!< Attached to struct proc.
+#define EVFILT_TIMER         (-7)      //!< Timers.
+#define EVFILT_NETDEV        (-8)      //!< Network devices.
+#define EVFILT_FS            (-9)      //!< Filesystem events.
+#define EVFILT_LIO           (-10)     //!< Attached to lio requests.
+#define EVFILT_USER          (-11)     //!< User events.
+#define EVFILT_SYSCOUNT        11
+/** @} */
 
-/*
- * data/hint flags for EVFILT_VNODE
+/** @name Actions
+ *
+ * @{
  */
-#define	NOTE_DELETE	0x0001			/* vnode was removed */
-#define	NOTE_WRITE	0x0002			/* data contents changed */
-#define	NOTE_EXTEND	0x0004			/* size increased */
-#define	NOTE_ATTRIB	0x0008			/* attributes changed */
-#define	NOTE_LINK	0x0010			/* link count changed */
-#define	NOTE_RENAME	0x0020			/* vnode was renamed */
-#define	NOTE_REVOKE	0x0040			/* vnode access was revoked */
-#undef  NOTE_REVOKE                 /* Not supported on Linux */
+#define EV_ADD          0x0001         //!< Add event to kq (implies enable).
+#define EV_DELETE       0x0002         //!< Delete event from kq.
+#define EV_ENABLE       0x0004         //!< Enable event.
+#define EV_DISABLE      0x0008         //!< Disable event (not reported).
+/** @} */
 
-/*
- * data/hint flags for EVFILT_PROC
+/** @name Flags
+ *
+ * @{
  */
-#define	NOTE_EXIT	0x80000000		/* process exited */
-#define	NOTE_FORK	0x40000000		/* process forked */
-#define	NOTE_EXEC	0x20000000		/* process exec'd */
-#define	NOTE_PCTRLMASK	0xf0000000		/* mask for hint bits */
-#define	NOTE_PDATAMASK	0x000fffff		/* mask for pid */
+#define EV_ONESHOT      0x0010         //!< Only report one occurrence.
+#define EV_CLEAR        0x0020         //!< Clear event state after reporting.
+#define EV_RECEIPT      0x0040         //!< Force EV_ERROR on success, data=0.
+#define EV_DISPATCH     0x0080         //!< Disable event after reporting.
 
-/* additional flags for EVFILT_PROC */
-#define	NOTE_TRACK	0x00000001		/* follow across forks */
-#define	NOTE_TRACKERR	0x00000002		/* could not track child */
-#define	NOTE_CHILD	0x00000004		/* am a child process */
+#define EV_SYSFLAGS     0xF000         //!< Reserved by system.
+#define EV_FLAG1        0x2000         //!< Filter-specific flag.
+/** @} */
 
-/*
- * data/hint flags for EVFILT_NETDEV
+/** @name Returned values
+ *
+ * @{
  */
-#define NOTE_LINKUP	0x0001			/* link is up */
-#define NOTE_LINKDOWN	0x0002			/* link is down */
-#define NOTE_LINKINV	0x0004			/* link state is invalid */
+#define EV_EOF          0x8000         //!< EOF detected.
+#define EV_ERROR        0x4000         //!< Error, data contains errno.
+/** @} */
 
-/* KLUDGE: This is from <sys/mount.h> on FreeBSD and is used by
-           the EVFILT_FS filter. */
-/* vfsquery flags */
-#define VQ_NOTRESP      0x0001  /* server down */
-#define VQ_NEEDAUTH     0x0002  /* server bad auth */
-#define VQ_LOWDISK      0x0004  /* we're low on space */
-#define VQ_MOUNT        0x0008  /* new filesystem arrived */
-#define VQ_UNMOUNT      0x0010  /* filesystem has left */
-#define VQ_DEAD         0x0020  /* filesystem is dead, needs force unmount */
-#define VQ_ASSIST       0x0040  /* filesystem needs assistance from external
-                                   program */
-#define VQ_NOTRESPLOCK  0x0080  /* server lockd down */
-
-/*
- * data/hint flags for EVFILT_TIMER as suported and defined in kevent64
+/** @name Data/hint flags/masks for EVFILT_USER
+ *
+ * On input, the top two bits of fflags specifies how the lower twenty four
+ * bits should be applied to the stored value of fflags.
+ *
+ * On output, the top two bits will always be set to NOTE_FFNOP and the
+ * remaining twenty four bits will contain the stored fflags value.
+ * @{
  */
-#define NOTE_SECONDS    0x0001  /* time specified in seconds */
-#define NOTE_USECONDS   0x0002  /* time specified in micro seconds */
-#define NOTE_NSECONDS   0x0004  /* time specified in nano seconds */
-#define NOTE_ABSOLUTE   0x0008  /* data is an absolute timeout */
+#define NOTE_FFNOP      0x00000000     //!< Ignore input fflags.
+#define NOTE_FFAND      0x40000000     //!< AND fflags.
+#define NOTE_FFOR       0x80000000     //!< OR fflags.
+#define NOTE_FFCOPY     0xc0000000     //!< Copy fflags.
+#define NOTE_FFCTRLMASK 0xc0000000     //!< Masks for operations.
+#define NOTE_FFLAGSMASK 0x00ffffff
 
+#define NOTE_TRIGGER    0x01000000     //!< Cause the event to be triggered for output.
+/** @} */
 
+/** @name Data/hint flags for EVFILT_{READ|WRITE}
+ *
+ * @{
+ */
+#define NOTE_LOWAT      0x0001         //!< Low water mark.
+#undef  NOTE_LOWAT      /* Not supported on Linux */
+/** @} */
+
+/** @name Data/hint flags for EVFILT_VNODE
+ *
+ * @{
+ */
+#define NOTE_DELETE     0x0001         //!< Vnode was removed.
+#define NOTE_WRITE      0x0002         //!< Data contents changed.
+#define NOTE_EXTEND     0x0004         //!< Size increased.
+#define NOTE_ATTRIB     0x0008         //!< Attributes changed.
+#define NOTE_LINK       0x0010         //!< Link count changed.
+#define NOTE_RENAME     0x0020         //!< Vnode was renamed.
+#define NOTE_REVOKE     0x0040         //!< Vnode access was revoked.
+#undef  NOTE_REVOKE     /* Not supported on Linux */
+/** @} */
+
+/** @name Data/hint flags for EVFILT_PROC
+ *
+ * @{
+ */
+#define    NOTE_EXIT    0x80000000     //!< Process exited.
+#define    NOTE_FORK    0x40000000     //!< Process forked.
+#define    NOTE_EXEC    0x20000000     //!< Process exec'd.
+#define    NOTE_PCTRLMASK 0xf0000000   //!< Mask for hint bits.
+#define    NOTE_PDATAMASK 0x000fffff   //!< Mask for pid.
+/** @} */
+
+/** @name Additional flags for EVFILT_PROC
+ *
+ * @{
+ */
+#define    NOTE_TRACK   0x00000001     //!< Follow across forks.
+#define    NOTE_TRACKERR 0x00000002    //!< Could not track child.
+#define    NOTE_CHILD   0x00000004     //!< Am a child process.
+/** @} */
+
+/** @name Data/hint flags for EVFILT_NETDEV
+ *
+ * @{
+ */
+#define NOTE_LINKUP     0x0001         //!< Link is up.
+#define NOTE_LINKDOWN   0x0002         //!< Link is down.
+#define NOTE_LINKINV    0x0004         //!< Link state is invalid.
+/** @} */
+
+/** @name vfsquery flags
+ *
+ * @note KLUDGE: This is from <sys/mount.h> on FreeBSD and is used by the EVFILT_FS filter.
+ *
+ * @{
+ */
+#define VQ_NOTRESP      0x0001         //!< Server down.
+#define VQ_NEEDAUTH     0x0002         //!< server bad auth.
+#define VQ_LOWDISK      0x0004         //!< we're low on space.
+#define VQ_MOUNT        0x0008         //!< new filesystem arrived.
+#define VQ_UNMOUNT      0x0010         //!< filesystem has left.
+#define VQ_DEAD         0x0020         //!< filesystem is dead, needs force unmount.
+#define VQ_ASSIST       0x0040         //!< filesystem needs assistance from external program.
+#define VQ_NOTRESPLOCK  0x0080         //!< server lockd down.
+/** @} */
+
+/** @name Data/hint flags for EVFILT_TIMER as suported and defined in kevent64
+ *
+ * @{
+ */
+#define NOTE_SECONDS    0x0001         //!< Time specified in seconds.
+#define NOTE_USECONDS   0x0002         //!< Time specified in micro seconds.
+#define NOTE_NSECONDS   0x0004         //!< Time specified in nano seconds.
+#define NOTE_ABSOLUTE   0x0008         //!< Data is an absolute timeout.
+/** @} */
 
 #ifndef __KERNEL__
 #ifdef  __cplusplus
@@ -199,8 +248,8 @@ kqueue(void);
 
 __declspec(dllexport) int
 kevent(int kq, const struct kevent *changelist, int nchanges,
-	    struct kevent *eventlist, int nevents,
-	    const struct timespec *timeout);
+        struct kevent *eventlist, int nevents,
+        const struct timespec *timeout);
 
 #ifdef MAKE_STATIC
 __declspec(dllexport) int
@@ -210,8 +259,8 @@ libkqueue_init();
 #else
 int     kqueue(void);
 int     kevent(int kq, const struct kevent *changelist, int nchanges,
-	    struct kevent *eventlist, int nevents,
-	    const struct timespec *timeout);
+        struct kevent *eventlist, int nevents,
+        const struct timespec *timeout);
 #ifdef MAKE_STATIC
 int     libkqueue_init();
 #endif
