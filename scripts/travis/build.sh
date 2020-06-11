a9596b88c3033b736c1080a7691fd656f2c4fe57
Jorge Pereira <jpereiran@gmail.com>
2020-06-11 15:29:49 -0300

    Enhance the Travis build
    * Builds against GCC and CLANG in Debug and Release mode.
    * Upgrade to Ubuntu 18.04

---
+++ scripts/travis/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+# Author: Jorge Pereira <jpereira@freeradius.org>
+#
+
+function fatal() {
+    echo "$0: ERROR: $@"
+    exit 1
+}
+
+if [[ ! "${BUILD_TYPE}" =~ Debug|Release ]]; then
+    fatal "The 'BUILD_TYPE' should be 'Debug' or 'Release'."
+fi
+
+if ! cmake . -G "Unix Makefiles" \
+    -DCMAKE_INSTALL_PREFIX="/usr" \
+    -DCMAKE_INSTALL_LIBDIR="lib"  \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL="ON" \
+    -DENABLE_TESTING="YES" \
+    -DENABLE_ASAN="${DENABLE_ASAN:-YES}" \
+    -DENABLE_LSAN="${DENABLE_LSAN:-YES}" \
+    -DENABLE_UBSAN="${DENABLE_UBSAN:-YES}" \
+    -DCMAKE_BUILD_TYPE="${BUILD_TYPE}"; then
+    fatal "Failed during cmake build configuration"
+fi
+
+#
+#  Build the libkqueue
+#
+echo "Starting compilation"
+if ! make -j8; then
+    fatal "Failed during compilation"
+fi
+
+#
+#  Build the *.deb packages
+#
+if ! cpack -G DEB; then
+    fatal "Failed when building debian packages"
+fi
